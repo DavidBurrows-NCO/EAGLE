c8ff674626cf86b1c1d1a5f3d221585a0fc24a12
D. Alex Burrows <David.Burrows@noaa.gov>
2026-01-06 14:04:44 +0000

    run fcst in an ops-like env

--- dev/scripts/exfcst.sh
+++ dev/scripts/exfcst.sh
@@ -1,1 +1,22 @@
-execute anemoi-inference
+#!/bin/bash
+set -x
+
+# Copy config file to DATA
+cp ${PARMeagle}/inference.yaml ${DATA}
+cp ${COMINeagle}/inference-last.ckpt ${DATA}
+cp -r ${COMINeagle}/gfs.zarr ${DATA}
+cp -r ${COMINeagle}/hrrr.zarr ${DATA}
+
+eagle-tools inference inference.yaml
+export err=$?; #err_chk
+
+if [[ ${err} -ne 0 ]]; then
+    echo "Aborted job with err=${err}"
+    exit ${err}
+fi
+
+# Copy output to COMOUTeagle
+#cp ${DATA}/conservative_719x1440_180x360.nc ${COMOUTeagle}
+#cp -r ${DATA}/gfs.zarr ${COMOUTeagle}
+
+exit
